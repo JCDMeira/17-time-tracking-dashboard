a49dcfab9584d9670ef66835d309c161fe2e2267
Jean Meira <jean.meira10@hotmail.com>
2021-12-05 22:26:06 -0300

    add: jsonServer and axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
-import React from 'react';
+/* eslint-disable no-unused-vars */
+import React, { useEffect, useState } from 'react';
 import { GlobalStyle } from './Global';
 
+import { api } from './services/api';
+
 function App() {
+  const [work, setWork] = useState([]);
+
+  useEffect(() => {
+    api.get('/work').then((response) => {
+      console.log(response);
+      console.log(response.data);
+      setWork(response.data);
+    });
+  }, []);
   return (
     <>
       <GlobalStyle />
       <h1>Hello world</h1>
+      <h2>{work.title}</h2>
     </>
   );
 }
